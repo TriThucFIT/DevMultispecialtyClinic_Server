e96ff85a2d2d1b5574c6f064d904159b44ec2fef
Bùi Trí Thức <125670011+BuiTriThuc150602@users.noreply.github.com>
2024-12-06 21:37:50 +0700

    Delete src/common directory

--- src/common/BaseDTO.ts
+++
@@ -1,9 +0,0 @@
-import { Expose, plainToInstance } from 'class-transformer';
-
-export abstract class BaseDTO {
-  id: number;
-  isActive: boolean;
-  static plainToInstance<T>(this: new (...args: any[]) => T, obj: T): T {
-    return plainToInstance(this, obj, { excludeExtraneousValues: true });
-  }
-}
